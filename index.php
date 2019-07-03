ac34158868dbf40b187aaa89d3e456085fa74897
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2019-07-02 22:28:13 -0400

    clip_path_lines

--- index.php
+++ index.php
@@ -1,68 +1,69 @@
-
-	<?php require("shared/header.php")?>
-	<section class="hero_content">
+<?php require("shared/header.php")?>
+<section class="hero_section">
+	<div class="hero_content">
 		<h1>C&amp;R first time customer free evaluation</h1>
 		<span><a href="contact.php?service=eva">Get Free Evaluation</a></span>
-	</section>
-	<!-- *****SERVICES***** -->
-	<section class="wide_skinny services">
-		<div class="flexbox">
-			<div class="flex_item">
-				<span>evaluation</span>
-			</div>
-			<div class="flex_item">
-				<span>preventive maintnace</span>
-			</div>
-			<div class="flex_item">
-				<span>installs</span>
-			</div>
-			<div class="flex_item">
-				<span>rebuilds</span>
-			</div>
-			<div class="flex_item">
-				<span>parts</span>
-			</div>
+	</div>
+</section>
+<!-- *****SERVICES***** -->
+<section class="wide_skinny services">
+	<div class="flexbox">
+		<div class="flex_item">
+			<span>evaluation</span>
 		</div>
-	</section>
-	<!-- *****SERVICE AREA***** -->
-	<section class="normal_content area">
-		<div class="grd_col_2">
-			<div class="grd_item_1">
-				<h3>Area</h3>
-				<p>We service Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum aliquid illo eveniet ducimus animi dicta ab, vitae voluptates autem accusamus quis, ipsa voluptatem consequuntur. Nostrum consequuntur sed deserunt libero tempore!</p>
-			</div>
-			<div class="grd_item_2">
-				<h3>contact us about service area</h3>
-				<span><a href="contact.php?service=area">Service area</a></span>
-			</div>
+		<div class="flex_item">
+			<span>preventive maintnace</span>
 		</div>
-	</section>
-	<!-- ******SERVICE TIME***** -->
-	<section class="normal_content time">
-		<div class="grd_col_2">
-			<div class="grd_item_1">
-				<h3>I need repair</h3>
-				<span><a href="contact.php?serive=repair">repair</a></span>
-			</div>
-			<div class="grd_item_2">
-				<h3>24/7 Emergancy repair</h3>
-			</div>
+		<div class="flex_item">
+			<span>installs</span>
 		</div>
-	</section>
-	<!-- ******ABOUT US***** -->
-	<section class="skinny_wide about">
-		<div class="grd_col_2">
-			<div class="grd_item_1">
-				<h3>Who We Are</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni facilis optio recusandae eum, omnis iusto. Ratione est, praesentium aspernatur, voluptatem minus pariatur cumque quo facilis omnis libero doloribus tenetur, ea!</p>
-			</div>
-			<div class="grd_item_2">
-				<h3>Have questions about us</h3>
-				<span><a href="contact.php?service=question">Questions</a></span>
-			</div>
+		<div class="flex_item">
+			<span>rebuilds</span>
 		</div>
-	</section>
-	<?php include("shared/footer.php")?>
+		<div class="flex_item">
+			<span>parts</span>
+		</div>
+	</div>
+</section>
+<!-- *****SERVICE AREA***** -->
+<section class="normal_content area">
+	<div class="grd_col_2">
+		<div class="grd_item_1">
+			<h3>Area</h3>
+			<p>We service Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum aliquid illo eveniet ducimus animi dicta ab, vitae voluptates autem accusamus quis, ipsa voluptatem consequuntur. Nostrum consequuntur sed deserunt libero tempore!</p>
+		</div>
+		<div class="grd_item_2">
+			<h3>contact us about service area</h3>
+			<span><a href="contact.php?service=area">Service area</a></span>
+		</div>
+	</div>
+</section>
+<!-- ******SERVICE TIME***** -->
+<section class="normal_content time">
+	<div class="grd_col_2">
+		<div class="grd_item_1">
+			<h3>I need repair</h3>
+			<span><a href="contact.php?serive=repair">repair</a></span>
+		</div>
+		<div class="grd_item_2">
+			<h3>24/7 Emergancy repair</h3>
+		</div>
+	</div>
+</section>
+<!-- ******ABOUT US***** -->
+<section class="skinny_wide about">
+	<div class="grd_col_2">
+		<div class="grd_item_1">
+			<h3>Who We Are</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni facilis optio recusandae eum, omnis iusto. Ratione est, praesentium aspernatur, voluptatem minus pariatur cumque quo facilis omnis libero doloribus tenetur, ea!</p>
+		</div>
+		<div class="grd_item_2">
+			<h3>Have questions about us</h3>
+			<span><a href="contact.php?service=question">Questions</a></span>
+		</div>
+	</div>
+</section>
+<?php include("shared/footer.php")?>
 </body>
 
 </html>
