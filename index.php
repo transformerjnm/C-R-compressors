003fffb262476bf01b4f851c2d39d629b661fbfc
Blaine137 <52352305+Blaine137@users.noreply.github.com>
2019-07-19 13:39:01 -0400

    on scroll animations

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require("shared/header.php")?>
 <section class="hero_section" id="hero_id">
 <!-- smoke effect-->
-<video autoplay loop muted>
+<video autoplay loop muted id="video">
     <source src="images/smoke.mp4" type="video/MP4">
 </video>
 	<div class="hero_content">
@@ -38,10 +38,10 @@
 <section class="normal_content area" id="area_id">
 	<div class="grd_col_2">
 		<div class="grd_item_1">
-			<h3>Area</h3>
-			<p>We service Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum aliquid illo eveniet ducimus animi dicta ab, vitae voluptates autem accusamus quis, ipsa voluptatem consequuntur. Nostrum consequuntur sed deserunt libero tempore!</p>
+			<h3 id="header_3">Area</h3>
+			<p id="paragraph">We service Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum aliquid illo eveniet ducimus animi dicta ab, vitae voluptates autem accusamus quis, ipsa voluptatem consequuntur. Nostrum consequuntur sed deserunt libero tempore!</p>
 		</div>
-		<div class="grd_item_2">
+		<div class="grd_item_2" id="contact_area">
 			<h3>contact us about service area</h3>
 			<span><a href="contact.php?service=area" class="contact_button">Service area</a></span>
 		</div>
@@ -50,11 +50,11 @@
 <!-- ******SERVICE TIME***** -->
 <section class="normal_content time" id="time_id">
 	<div class="grd_col_2">
-		<div class="grd_item_1">
+		<div class="grd_item_1" id="i_need_repair">
 			<h3>I need repair</h3>
 			<span><a href="contact.php?serive=repair" class="contact_button">repair</a></span>
 		</div>
-		<div class="grd_item_2">
+		<div class="grd_item_2" id="emergancy">
 			<h3>24/7 Emergancy repair</h3>
 		</div>
 	</div>
