85f77a222dcf376a8127c50a145c112192186564
Blaine137 <52352305+Blaine137@users.noreply.github.com>
2019-07-18 22:39:21 -0400

    video playback rate

--- js/animate.js
+++ js/animate.js
@@ -1,29 +1,38 @@
-
         //---------title animation----------
 window.addEventListener('load', function(){
         var title = document.getElementById("title");
         var button = document.getElementById("freeEvaluation");
         var services = document.getElementById("services_animate");
           //title animation
-        title.className = "fade-in";      
+        title.className = "fade-in";
+      
+          
+        
     });
 
-//------scroll animations-------
-const boxes = document.querySelector("div.flex_item");
+// ------ add playback time to smoke video ------
+let video = document.getElementById("video");
+
+video.playbackRate = 0.5; //changes the rate the video plays
+
+
+
+
+        //------scroll animations-------
+let boxes = document.querySelectorAll('.flex_item');
+
 
 window.addEventListener('scroll', () => {
         
         // services animation below
-    console.log(window.innerHeight); //space between the top and bottom of the browser 524
     
-    console.log(boxes.getBoundingClientRect().top); // boxes are in viewport at 422
+
+    
+    
+    
+
     
 }); //end of scroll event
 
-//functions
 
-function animatesmoke(){
-    
-    
-    
-}
+
