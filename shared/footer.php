690ed3cdfd2a65b8dccbee69be336d2b9f96c600
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2019-12-28 18:40:27 -0500

    final major styles

--- shared/footer.php
+++ shared/footer.php
@@ -15,16 +15,17 @@
 				<ul>
 					<li><a href="mailto:asdasdasdasdasd">Email: adsasdasdasdasd</a></li>
 					<li><a href="tel:1231231234">Phone: 123-123-1234</a></li>
-					<span>
+					<li style="text-align: center"><span >
 						<a  id="footer_btn" class="hide_mobile contact_button fade_in2" href="mailto:email" >Contact</a>
 						<a id="footer_btn" class="hide_desktop contact_button fade_in2" href="tel:phone" >Contact</a>
-					</span>
-					<li>
+					</span></li>
+					<li style="text-align: center">
 						<a href="https://www.facebook.com/CandRCompressor/" rel="nofollow" target="_blank"><i class="fab fa-facebook-square"></i></a>
 					<li>
 				</ul>
 			</div>
 		</div>
+		<img class="fade_in2" src="images/r&c_logo.png" alt="C&R Compressors logo" class="logo"><br>
 	<span>Copyright &copy; C&amp;R Compressors 2019</span>
 </footer>
 <script src="js/animate.js">
